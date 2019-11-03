0998c0bc5f898e853ab7290e46987f8516fbe848
Adrian Helvik <adrian.helvik@specifique.no>
2019-11-03 01:55:53 +0100

    Treat arrays as fragments

---
+++ src/bugfixes.spec.js
@@ -0,0 +1,37 @@
+import fragdom from '@adrianhelvik/fragdom'
+import createElement from './createElement'
+import createDiff from './createDiff'
+import applyDiff from './applyDiff'
+
+const prevTemplates = new WeakMap()
+const mount = (curr, target) => {
+  let prev = prevTemplates.get(target)
+  if (prev === undefined) {
+    prev = null
+  }
+  const diff = createDiff(prev, curr)
+  prevTemplates.set(target, diff)
+  applyDiff(target, diff)
+}
+
+test('repro', () => {
+  const root = document.createElement('div')
+
+  mount([<div class="todoName">By "{''}"</div>], root)
+  mount([<div class="todoName">By "{'A nice name'}"</div>], root)
+
+  const todoNameNode = root.querySelector('.todoName')
+  const wrappedTodoNameNode = fragdom.wrap(todoNameNode)
+
+  expect(wrappedTodoNameNode.debug()).toBe(
+    [
+      /*****************************/
+      '<div class={todoName}>',
+      '  By "',
+      '  A nice name',
+      '  "',
+      '</div>',
+    ].join('\n'),
+  )
+  expect(todoNameNode.textContent).toBe('By "A nice name"')
+})
--- src/createDiff.js
+++ src/createDiff.js
@@ -1,8 +1,16 @@
+import createElement from './createElement.js'
 import diffNodes from './diffNodes.js'
 import diffProps from './diffProps.js'
 
 export default function diff(prev, curr) {
   const diff = []
+
+  if (Array.isArray(prev)) {
+    prev = createElement(createElement.Fragment, {}, ...prev)
+  }
+  if (Array.isArray(curr)) {
+    curr = createElement(createElement.Fragment, {}, ...curr)
+  }
 
   diff.push(...diffNodes(prev, curr, [0]))
 
